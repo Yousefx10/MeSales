cdca4c6a1bad4484d96f1ddde1db045b8caef1cf
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-11-16 02:04:49 +0300

    adding results column

--- dashboard.php
+++ dashboard.php
@@ -9,13 +9,21 @@
 
     </head>
     <body>
+    <div style="float:left;border:1px solid #000;width:19%;height:90%;text-align:center;margin-right:10px">
+            <h3>Results</h3>
+    </div>
 
-        <div style="float: left;border:2px solid #000;width:79%;height:90%;text-align:center">
+        <span style="clear: both;"></span>
+        <div style="float: left;border:2px solid #000;width:60%;height:90%;text-align:center">
             <h2>Main Menu</h2>
         </div>
         <div style="float: right;border:1px solid #000;width:19%;height:90%;text-align:center">
             <p>Select Option</p>
         </div>
+
+
+
+
 
     </body>
 </html>
