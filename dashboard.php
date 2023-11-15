3718a8a20d15374d29f1b7407598eb9f0bc19e3b
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-11-16 01:59:24 +0300

    normal styles

--- dashboard.php
+++ dashboard.php
@@ -2,6 +2,20 @@
 <html>
     <head>
         <title>Dashboard</title>
+        <style>
+            body{margin: auto;height: 100%;}
+            html{height: 100%;}
+        </style>
+
     </head>
-    <body></body>
+    <body>
+
+        <div style="float: left;border:2px solid #000;width:79%;height:90%;text-align:center">
+            <h2>Main Menu</h2>
+        </div>
+        <div style="float: right;border:1px solid #000;width:19%;height:90%;text-align:center">
+            <p>Select Option</p>
+        </div>
+
+    </body>
 </html>
