c51254f084da8d6b5f5b3dea142fbf816a71aa8d
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-11-18 00:32:03 +0300

    adding more styles

--- dashboard.php
+++ dashboard.php
@@ -5,20 +5,46 @@
         <style>
             body{margin: auto;height: 100%;}
             html{height: 100%;}
-            .btn-options{border: none;padding: 15px;width:80%;}
+            .btn-options{border: none;padding: 15px;width:80%;cursor:pointer;font-weight: bold;}
             .btn-options:hover{background-color: darkseagreen;}
+
+            table{padding: 10px;}
+            table tr{}
         </style>
 
     </head>
     <body>
+        <!--Results -->
     <div style="float:left;border:1px solid #000;width:19%;height:90%;text-align:center;margin-right:10px">
             <h3>Results</h3>
     </div>
 
         <span style="clear: both;"></span>
+
+
+
+        <!--Main Menu-->
         <div style="float: left;border:2px solid #000;width:60%;height:90%;text-align:center">
             <h2>Main Menu</h2>
+            
+            <table border="1px">
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            </table>
         </div>
+
+
+
+
+        <!--Options-->
         <div style="float: right;border:1px solid #000;width:19%;height:90%;text-align:center">
             <p>Select Option</p>
             <br/><br/>
