82bd6313a6e0a9b395c0433d051d4a308e7ccd7e
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-11-18 01:14:33 +0300

    Adding Food Examples

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@
 
             table{padding: 10px;margin-left: auto;margin-right: auto;}
             table tr td{height: 100px;width: 100px;padding: 15px;}
-            .lbltxt{display:block;height:inherit;overflow: hidden;}
+            .lbltxt{display:block;height:inherit;overflow: hidden;text-overflow: ellipsis;}
         </style>
 
     </head>
@@ -30,45 +30,45 @@
             <div style="overflow-y: scroll;height:inherit;">
                 <table border="1px">
                     <tr>
-                        <td><span class="lbltxt">click on me so you won't lose anything</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
+                        <td><span class="lbltxt">Chocolate</span></td>
+                        <td><span class="lbltxt">Cheese</span></td>
+                        <td><span class="lbltxt">Water</span></td>
+                        <td><span class="lbltxt">Milk</span></td>
+                        <td><span class="lbltxt">Donut</span></td>
+                        <td><span class="lbltxt">Lemon</span></td>
                     </tr>
                     <tr>
-                        <td><span class="lbltxt">click on me so you won't lose anythingclick on me so you won't lose anythingclick on me so you won't lose anything</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
+                        <td><span class="lbltxt">Caramel</span></td>
+                        <td><span class="lbltxt">Tomato</span></td>
+                        <td><span class="lbltxt">Flower</span></td>
+                        <td><span class="lbltxt">Oil</span></td>
+                        <td><span class="lbltxt">Biscuit</span></td>
+                        <td><span class="lbltxt">Pizza</span></td>
                     </tr>
                     <tr>
-                        <td><span class="lbltxt">1click on me so you won't lose anythingclick on me so you won't lose anythingclick on me so you won't lose anythingclick on me so you won't lose anything12</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
+                        <td><span class="lbltxt">Meat</span></td>
+                        <td><span class="lbltxt">Sandwish</span></td>
+                        <td><span class="lbltxt">Bread</span></td>
+                        <td><span class="lbltxt">Eggs</span></td>
+                        <td><span class="lbltxt">Seafood</span></td>
+                        <td><span class="lbltxt">Cream</span></td>
                     </tr>
                     <tr>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
-                        <td><span class="lbltxt">1</span></td>
+                        <td><span class="lbltxt">Yogurt</span></td>
+                        <td><span class="lbltxt">Butter</span></td>
+                        <td><span class="lbltxt">Apple</span></td>
+                        <td><span class="lbltxt">Orange</span></td>
+                        <td><span class="lbltxt">Peach</span></td>
+                        <td><span class="lbltxt">Banana</span></td>
                     </tr>
 
                     <tr>
-                        <td><span class="lbltxt">2</span></td>
-                        <td><span class="lbltxt">2</span></td>
-                        <td><span class="lbltxt">2</span></td>
-                        <td><span class="lbltxt">2</span></td>
-                        <td><span class="lbltxt">2</span></td>
-                        <td><span class="lbltxt">2</span></td>
+                        <td><span class="lbltxt">Pear</span></td>
+                        <td><span class="lbltxt">Watermelon</span></td>
+                        <td><span class="lbltxt">Mango</span></td>
+                        <td><span class="lbltxt">Cherry</span></td>
+                        <td><span class="lbltxt">Raspberry</span></td>
+                        <td><span class="lbltxt">Pineapple</span></td>
                     </tr>
 
                 </table>
@@ -80,7 +80,7 @@
 
         <!--Options-->
         <div style="float: right;border:1px solid #000;width:19%;height:90%;text-align:center">
-            <p>Select Option</p>
+            <h3 >Select Option</h3>
             <br/><br/>
             <button class="btn-options">Click ME</button>
         </div>
