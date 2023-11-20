6b5da08c751f7d912ff1d2b6a5833d14704f8f21
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2023-11-21 01:12:52 +0300

    remove if swiped left

--- script.js
+++ script.js
@@ -18,10 +18,14 @@ div.addEventListener('mousedown', function(e) {
     ];
 }, true);
 
+
+var oldx = 0;
+
 document.addEventListener('mouseup', function() {
     isDown = false;
     div.style.margin ='auto';
    div.style.position = "sticky";
+
 }, true);
 
 document.addEventListener('mousemove', function(event) {
@@ -35,8 +39,58 @@ document.addEventListener('mousemove', function(event) {
 
     //    console.log("A"+offset[0]);
         div.style.position = "relative";
-
         div.style.left = (mousePosition.x + offset[0]) + 'px';
+
+
         div.style.margin ='0';
+
+
+        console.log("ddd");
+  
     }
 }, true);
+
+
+
+const swipeableDiv = document.getElementById('test');
+let startX = 0;
+let endX = 0;
+
+swipeableDiv.addEventListener('mousedown', handleStart);
+swipeableDiv.addEventListener('touchstart', handleStart);
+
+function handleStart(event) {
+    if (event.type === 'mousedown') {
+        startX = event.clientX;
+        document.addEventListener('mousemove', handleMove);
+        document.addEventListener('mouseup', handleEnd);
+    } else if (event.type === 'touchstart') {
+        startX = event.touches[0].clientX;
+        document.addEventListener('touchmove', handleMove);
+        document.addEventListener('touchend', handleEnd);
+    }
+}
+
+function handleMove(event) {
+    endX = event.type === 'mousemove' ? event.clientX : event.touches[0].clientX;
+}
+
+function handleEnd() {
+    const swipeDistance = startX - endX;
+
+    // You can adjust this threshold to determine when to consider it a swipe
+    const swipeThreshold = 50;
+
+    if (Math.abs(swipeDistance) > swipeThreshold) {
+        // Swipe left or right detected, delete the div
+        swipeableDiv.remove();
+    }
+
+    // Reset coordinates and remove event listeners
+    startX = 0;
+    endX = 0;
+    document.removeEventListener('mousemove', handleMove);
+    document.removeEventListener('mouseup', handleEnd);
+    document.removeEventListener('touchmove', handleMove);
+    document.removeEventListener('touchend', handleEnd);
+}
